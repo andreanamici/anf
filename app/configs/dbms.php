bddf70f64a1b5ca0430ef251848134da849a1beb
lucatarik <lucatarik@gmail.com>
2017-02-28 21:32:37 +0100

    revert dbms

--- app/configs/dbms.php
+++ app/configs/dbms.php
@@ -10,21 +10,21 @@ define("DB_MANAGER_CONFIG_DEFAULT", "default");
  */
 define("DB_MANAGER_CONFIGS", serialize(Array(
     'default' => Array(
-        'user'     => 'root',
-        'password' => '',
-        'port'     => '3306',
-        'dbname'   => 'anf',
-        'host'     => '127.0.0.1',
-        'driver'   => 'mysql',
-        'charset'  => 'utf8',
+        'user' => false,
+        'password' => false,
+        'port' => '3306',
+        'dbname' => '',
+        'host' => '',
+        'driver' => 'mysql',
+        'charset' => 'utf8',
         'persistent' => true,
         'writelog' => LOGS_ENABLE,
         'table_prefix' => ''
     ),
     'sqlite_memory' => Array(
         'dbname' => ':memory:',
-        'driver' => 'sqlite',
-        'writelog' => LOGS_ENABLE,
+        'driver' => 'sqlite',       
+        'writelog' => LOGS_ENABLE,       
         'table_prefix' => ''
     ),
 //     'other'    => Array(
